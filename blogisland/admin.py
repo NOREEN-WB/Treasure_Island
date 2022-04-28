0995a8d5ec0f73cec3526164b0a24c008354655e
noreen-wb <nwbutt@hotmail.com>
2022-04-29 01:46:53 +0500

    create view for Postlist

--- blogisland/admin.py
+++ blogisland/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comment
 
 
 # Register your models here.
@@ -14,3 +14,11 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """CommentAdmin Class"""
+    list_display = ['name', 'body', 'post', 'created_on']
+    list_filter = ['created_on']
+    search_fields = ['name', 'email', 'body']
