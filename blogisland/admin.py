a781d3149c29d4a706952616232bc93d2736c5f4
noreen-wb <nwbutt@hotmail.com>
2022-04-29 01:33:20 +0500

    Post Model to Admin Site

--- blogisland/admin.py
+++ blogisland/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Post
 
 
 # Register your models here.
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """PostAdmin class"""
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content',)
