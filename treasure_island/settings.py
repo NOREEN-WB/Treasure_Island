9ead31b68a656a6a16cb6601c60af9145b2235be
noreen-wb <nwbutt@hotmail.com>
2022-04-29 00:50:57 +0500

    added secret key and database

--- treasure_island/settings.py
+++ treasure_island/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+9j^t$8muwzc&19ea8#iw702&g68*#o=$6f%z+&2n4_sg_q8k3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'treasure_island.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
