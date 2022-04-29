ed300a1f83c5c91e31d5a4e53bae872450089692
noreen-wb <nwbutt@hotmail.com>
2022-04-29 12:19:36 +0500

    likes

--- blogisland/views.py
+++ blogisland/views.py
@@ -1,5 +1,7 @@
+"""Imports"""
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 # Create your views here.
@@ -58,3 +60,14 @@ class PostDetail(View):
             "comment_form": CommentForm()
         },)
 
+class PostLike(View):
+    """PostLike Class View"""
+    def post(self, request, slug):
+        """post function"""
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
