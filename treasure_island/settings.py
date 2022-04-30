94a9d9189799245874ec8a0098eb23dee44ef8d0
noreen-wb <nwbutt@hotmail.com>
2022-04-30 08:53:38 +0500

    settings.py move siteId , login,logout redirect url from Middleware

--- treasure_island/settings.py
+++ treasure_island/settings.py
@@ -57,6 +57,12 @@ INSTALLED_APPS = [
     'blogisland',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -67,10 +73,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SITE_ID = 1
-
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
